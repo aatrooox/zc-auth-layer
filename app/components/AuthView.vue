7b5f5455e09e2b93d1223ff90f1d2a069ebc21f7
gnak <gnakzz@qq.com>
2025-01-23 16:43:52 +0800

    feat: 提示信息

--- app/components/AuthView.vue
+++ app/components/AuthView.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <h1>Auth View Component</h1>
-    <div>
-      current auth status: {{ status }}
-    </div>
+    如果你看到了这条信息，说明此时我正在测试 Nuxt Layers (DDD) 之 <a href="https://github.com/aatrooox/zc-auth-layer">Auth Layer</a> 的功能。 GITHUB: {{ status }}
   </div>
 </template>
 <script lang="ts" setup>

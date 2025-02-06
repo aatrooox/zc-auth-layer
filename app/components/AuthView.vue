a90c5097e2c29b645d0c00e77d1b7afa2ca8d1bd
gnak <gnakzz@qq.com>
2025-02-06 11:43:09 +0800

    docs

--- app/components/AuthView.vue
+++ app/components/AuthView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    如果你看到了这条信息，说明此时我正在测试 Nuxt Layers (DDD) 之 <a href="https://github.com/aatrooox/zc-auth-layer">Auth Layer</a> 的功能。 GITHUB: {{ status }}
+    如果你看到了这条信息，说明此时我正在测试 Nuxt Layers: <a href="https://github.com/aatrooox/zc-auth-layer">Auth Layer</a> 的功能。 GITHUB: {{ status }}
   </div>
 </template>
 <script lang="ts" setup>
